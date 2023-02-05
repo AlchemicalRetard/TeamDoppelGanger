2f942dff6f028dafd930fb1c72bc109a83604a0b
Dev Rajpal <64584337+AlchemicalRetard@users.noreply.github.com>
2023-02-06 01:57:14 +0530

    Update powerUp.cs

--- powerUp.cs
+++ powerUp.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class powerUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int collisionCount = 0;
+    float powerUpSpeed=0f;
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        collisionCount++;
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (collisionCount > 10)
+        {
+            powerUpSpeed = GetComponent<playerMove>().walkSpeed;
+            GetComponent<playerMove>().walkSpeed = powerUpSpeed * 2;
+
+        }
+                
     }
 }
